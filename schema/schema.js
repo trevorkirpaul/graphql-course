74df9160f5794b8f884ebaf40c6378e34b66fa86
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-04-28 15:59:07 -0400

    added json server and changed root query to access it

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql')
-const _ = require('lodash')
+const axios = require('axios')
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -16,11 +16,6 @@ const UserType = new GraphQLObjectType({
   }
 })
 
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 },
-]
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -28,7 +23,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
