bc50d01cdede6a735e55a6e3dac1f898fd76bcfd
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-04-28 15:33:21 -0400

    added schema

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
-
+const schema = require('./schema/schema')
 const app = express()
 const port = 4000
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
 }))
 
