7d08ca27150b6aa6b00b0a8ea2a72dd4c93020c1
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-05-02 15:52:34 -0400

    added rootquery for companies, linked User company prop to Company

--- schema/schema.js
+++ schema/schema.js
@@ -7,12 +7,29 @@ const {
   GraphQLSchema
 } = graphql
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString} 
+  }
+})
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString  },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data)
+      }
+     }
   }
 })
 
@@ -26,6 +43,15 @@ const RootQuery = new GraphQLObjectType({
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
           .then(res => res.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(res => res.data)
       }
     }
   }
